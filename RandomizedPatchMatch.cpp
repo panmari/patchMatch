e84356891578c8c88710c7a02c448ccacbf39b0a
panmari <smmuzi@gmail.com>
2015-10-02 16:01:17 +0200

    Nornmalizing distance measure, being smarter about boarders.

--- RandomizedPatchMatch.cpp
+++ RandomizedPatchMatch.cpp
@@ -20,44 +20,55 @@ cv::Mat RandomizedPatchMatch::match(int patchSize) {
 
     Rect rectFullImg(Point(0,0), _img2.size());
 
-    for (int x = 1; x < _img.cols - patchSize; x++) {
-        for (int y = 1; y < _img.rows - patchSize; y++) {
+    for (int x = 0; x < _img.cols - patchSize; x++) {
+        for (int y = 0; y < _img.rows - patchSize; y++) {
             Rect currentPatchRect(x, y, patchSize, patchSize);
             Mat currentPatch = _img(currentPatchRect);
             Vec3f currentOffsetEntry = _offset_map.at<Vec3f>(y, x);
 
-
-            Vec3f offsetLeft = _offset_map.at<Vec3f>(y, x-1);
-            Rect rectLeft((int) offsetLeft[0] + x, (int) offsetLeft[1] + y, patchSize, patchSize);
-            if ((rectLeft & rectFullImg) == rectLeft) {
-                Mat matchingPatchLeft = _img2(rectLeft);
-                float leftSsd = (float) ssd(currentPatch, matchingPatchLeft);
-                if (leftSsd < currentOffsetEntry[2]) {
-                    _offset_map.at<Vec3f>(y, x) = Vec3f(offsetLeft[0], offsetLeft[1], leftSsd);
+            if (x > 0) {
+                Vec3f offsetLeft = _offset_map.at<Vec3f>(y, x - 1);
+                Rect rectLeft((int) offsetLeft[0] + x, (int) offsetLeft[1] + y, patchSize, patchSize);
+                if ((rectLeft & rectFullImg) == rectLeft) {
+                    Mat matchingPatchLeft = _img2(rectLeft);
+                    float leftSsd = (float) ssd(currentPatch, matchingPatchLeft);
+                    if (leftSsd < currentOffsetEntry[2]) {
+                        _offset_map.at<Vec3f>(y, x) = Vec3f(offsetLeft[0], offsetLeft[1], leftSsd);
+                    }
                 }
             }
-
-            Vec3f offsetUp = _offset_map.at<Vec3f>(y - 1, x);
-            Rect rectUp((int) offsetUp[0] + x, (int) offsetUp[1] + y, patchSize, patchSize);
-            if ((rectUp & rectFullImg) == rectUp) { // Check if it's fully inside
-                Mat matchingPatchUp = _img2(rectUp);
-                float upSsd = (float) ssd(currentPatch, matchingPatchUp);
-                if (upSsd < currentOffsetEntry[2]) {
-                    _offset_map.at<Vec3f>(y, x) = Vec3f(offsetUp[0], offsetUp[1], upSsd);
+            if (y > 0) {
+                Vec3f offsetUp = _offset_map.at<Vec3f>(y - 1, x);
+                Rect rectUp((int) offsetUp[0] + x, (int) offsetUp[1] + y, patchSize, patchSize);
+                if ((rectUp & rectFullImg) == rectUp) { // Check if it's fully inside
+                    Mat matchingPatchUp = _img2(rectUp);
+                    float upSsd = (float) ssd(currentPatch, matchingPatchUp);
+                    if (upSsd < currentOffsetEntry[2]) {
+                        _offset_map.at<Vec3f>(y, x) = Vec3f(offsetUp[0], offsetUp[1], upSsd);
+                    }
                 }
             }
-
         }
     }
     return _offset_map;
 }
 
 double RandomizedPatchMatch::ssd(cv::Mat &patch, cv::Mat &patch2) const {
-    Mat diff;
-    addWeighted(patch, 1, patch2, -1, 0, diff);
-    diff.mul(diff);
-    Scalar ssd_channels = sum(diff);
-    return ssd_channels[0] + ssd_channels[1] + ssd_channels[2];
+    Mat tmp;
+    addWeighted(patch, 1, patch2, -1, 0, tmp);
+    tmp.mul(tmp);
+    Scalar ssd_channels = sum(tmp);
+
+    patch.copyTo(tmp);
+    patch.mul(tmp);
+    Scalar squares_patch = sum(tmp);
+
+    patch2.copyTo(tmp);
+    patch2.mul(tmp);
+    Scalar squares_patch2 = sum(tmp);
+    double normalization = sqrt((squares_patch[0] + squares_patch[1] + squares_patch[2]) *
+                                        (squares_patch2[0] + squares_patch2[1] + squares_patch2[2]));
+    return (ssd_channels[0] + ssd_channels[1] + ssd_channels[2])/normalization;
 }
 
 void RandomizedPatchMatch::initializeOffsets(int patchSize) {
