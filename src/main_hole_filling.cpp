7ad9fa993fbbb3edbe63a473f0fbbbfb0f5ee615
panmari <smmuzi@gmail.com>
2015-10-15 17:35:37 +0200

    Can now pass original to hole filling main.

--- src/main_hole_filling.cpp
+++ src/main_hole_filling.cpp
@@ -22,11 +22,12 @@ const int PATCH_SIZE = 7;
 
 /**
  * Takes one image with a 'hole region' (pixels in magenta) as input. The hole region will then be inpainted.
+ * If a second image is given as argument, the ssd between the reconstructed one and this one will be printed to stdout.
  */
 int main( int argc, char** argv )
 {
     /// Load image
-    Mat source = imread( argv[1], 1 );
+    Mat source = imread( argv[1]);
 
     if (!source.data) {
         printf("Need a picture with a magenta region as arguments.\n");
@@ -45,5 +46,20 @@ int main( int argc, char** argv )
     Mat filled = hf.run();
 
     imwrite_lab("result.exr", filled);
+
+    if (argc == 3) {
+        Mat original = imread(argv[2]);
+        if (!original.data) {
+            printf("Failed to read comparison image.\n");
+            return -2;
+        }
+        resize(original, original, Size(), RESIZE_FACTOR, RESIZE_FACTOR);
+        original.convertTo(original, CV_32FC3, 1 / 255.f);
+
+        Mat filled_bgr;
+        cvtColor(filled, filled_bgr, CV_Lab2BGR);
+
+        cout << "SSD between original and hole filled version: " << ssd(original, filled_bgr) << endl;
+    }
     return 0;
 }
