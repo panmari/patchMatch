79ce8b1055a3e1502b32ac1894280e06de2de057
panmari <smmuzi@gmail.com>
2015-10-19 20:25:47 +0200

    Can now do exhaustive patch match on cpu.
    Also started adding some cmake code to make compilation possible without
    cuda.

---
+++ src/patch_match_provider/cpu/ExhaustivePatchMatch.cpp
@@ -0,0 +1,47 @@
+#include "ExhaustivePatchMatch.h"
+#include <boost/progress.hpp>
+#include "boost/iostreams/stream.hpp"
+
+using cv::Mat;
+using cv::matchTemplate;
+using cv::minMaxLoc;
+using cv::Point;
+using cv::Rect;
+using cv::Scalar;
+using cv::Vec3f;
+
+ExhaustivePatchMatch::ExhaustivePatchMatch(const Mat &source, const Mat &target, const int patch_size,
+                                           const bool show_progress_bar) : _source(source), _target(target),
+        _patch_size(patch_size), _show_progress_bar(show_progress_bar) {
+    _temp.create(source.size(), CV_32FC1);
+}
+
+Mat ExhaustivePatchMatch::match() {
+    Mat offset_map;
+	offset_map.create(_target.rows - _patch_size, _target.cols - _patch_size, CV_32FC3);
+
+	const unsigned long matched_pixels = static_cast<unsigned long>(offset_map.cols * offset_map.rows);
+
+    boost::iostreams::stream<boost::iostreams::null_sink> nullout { boost::iostreams::null_sink{} };
+    std::ostream& out = _show_progress_bar ? std::cout : nullout;
+    boost::progress_display show_progress(matched_pixels, out);
+
+	for (int x = 0; x < offset_map.cols; x++) {
+		for (int y = 0; y < offset_map.rows; y++) {
+			Rect rect(x, y, _patch_size, _patch_size);
+            Mat patch = _target(rect);
+            double minVal; Point minLoc;
+            matchSinglePatch(patch, &minVal, &minLoc);
+			offset_map.at<Vec3f>(y, x) = Vec3f(minLoc.x - x, minLoc.y - y, static_cast<float>(minVal));
+        }
+        show_progress += offset_map.rows;
+    }
+	return offset_map;
+}
+
+void ExhaustivePatchMatch::matchSinglePatch(const Mat &patch, double *minVal, Point *minLoc) const {
+    // Do the Matching
+    matchTemplate(_source, patch, _temp, cv::TM_SQDIFF);
+    // Localizing the best match with minMaxLoc
+    minMaxLoc(_temp, minVal, nullptr, minLoc, nullptr);
+}
---
+++ src/patch_match_provider/cpu/ExhaustivePatchMatch.h
@@ -0,0 +1,23 @@
+#ifndef PATCHMATCH_EXHAUSTIVEPATCHMATCH_H
+#define PATCHMATCH_EXHAUSTIVEPATCHMATCH_H
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "../PatchMatchProvider.h"
+
+class ExhaustivePatchMatch : public PatchMatchProvider {
+
+public:
+	ExhaustivePatchMatch(const cv::Mat &source, const cv::Mat &target, int patch_size, bool show_progress_bar = false);
+    cv::Mat match();
+
+private:
+    bool _show_progress_bar;
+	int _patch_size;
+    const cv::Mat _source, _target;
+    cv::Mat _temp;
+
+    void matchSinglePatch(const cv::Mat &patch, double *minVal, cv::Point *minLoc) const;
+};
+
+
+#endif //PATCHMATCH_EXHAUSTIVEPATCHMATCH_H
